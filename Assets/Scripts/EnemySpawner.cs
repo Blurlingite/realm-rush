34874774e2238b7b7d3c15d4b6282a0f5e342cb1
Vishnu Dawah <vkdawah@gmail.com>
2019-10-15 16:02:54 -0400

    Towers now shoot the closest enemy

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+  [Range(0.1f, 120f)]
+  [SerializeField] float secondsBetweenSpawns = 4f;
+
+  [SerializeField] EnemyMovement enemyPrefab;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    StartCoroutine(RepeatedlySpawnEnemies());
+  }
+
+  IEnumerator RepeatedlySpawnEnemies()
+  {
+    while (true)    // forever
+    {
+      Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+      yield return new WaitForSeconds(secondsBetweenSpawns);
+
+    }
+  }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,14 +7,17 @@ public class Tower : MonoBehaviour
 {
 
   [SerializeField] Transform objectToPan;
-  [SerializeField] Transform targetEnemy;
   [SerializeField] float attackRange;
   [SerializeField] ParticleSystem projectileParticle;
 
+  // State of each tower
+  Transform targetEnemy;
 
   // Update is called once per frame
   void Update()
   {
+    SetTargetEnemy();
+
     if (targetEnemy)
     {
       objectToPan.LookAt(targetEnemy);
@@ -24,6 +27,41 @@ public class Tower : MonoBehaviour
     {
       Shoot(false);
     }
+
+  }
+
+  private void SetTargetEnemy()
+  {
+    // find all enemies (by searching for their EnemyDamage script)
+    EnemyDamage[] sceneEnemies = FindObjectsOfType<EnemyDamage>();
+
+    if (sceneEnemies.Length == 0) { return; }
+
+    Transform closestEnemy = sceneEnemies[0].transform;
+
+    foreach (EnemyDamage testEnemy in sceneEnemies)
+    {
+      closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+    }
+
+    targetEnemy = closestEnemy;
+  }
+
+  private Transform GetClosest(Transform transformA, Transform transformB)
+  {
+    // get distance from this Tower to the transform of the 1st argument you passed in
+    var distToA = Vector3.Distance(transform.position, transformA.position);
+
+    var distToB = Vector3.Distance(transform.position, transformB.position);
+
+    if (distToA < distToB)
+    {
+      return transformA;
+    }
+
+    // otherwise, return transformB
+    return transformB;
+
 
   }
 
