80cffb9efe2101ee0e06c27acf87fee1f5653a43
Vishnu Dawah <vkdawah@gmail.com>
2020-02-29 21:15:56 -0500

    Added explosion animation and sound effect when Player's base is destroyed

---
+++ Assets/Scripts/DestroyBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBase : MonoBehaviour
+{
+
+    [SerializeField] PlayerHealth friendlyBase;
+    [SerializeField] GameObject explosionPrefab;
+    bool stopLooping = false;
+
+    void Update()
+    {
+        int playerHealth = friendlyBase.getPlayerHealth();
+
+        if (playerHealth == 0 && stopLooping == false)
+        {
+            StartCoroutine(DestroyFriendlyBase());
+        }
+
+    }
+
+
+    IEnumerator DestroyFriendlyBase()
+    {
+        stopLooping = true;
+
+        Vector3 explosionPosition = new Vector3(-0.3f, 5f, 6.4f);
+
+        Instantiate(explosionPrefab, explosionPosition, Quaternion.identity);
+        yield return new WaitForSeconds(0.2f);
+
+        friendlyBase.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.3f);
+
+        GetComponent<AudioSource>().Play();
+
+        yield return new WaitForSeconds(1.5f);
+
+        GameObject.Find("AA Theme Reversed").GetComponent<AudioSource>().Stop();
+
+        Time.timeScale = 0.0f;
+
+        this.enabled = false;
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,25 +6,33 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
 
-  [SerializeField] int health = 10;
-  [SerializeField] int healthDecrease = 1;
-  [SerializeField] Text healthText;
-  [SerializeField] AudioClip playerDamageSFX;
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText;
+    [SerializeField] AudioClip playerDamageSFX;
+    [SerializeField] ParticleSystem explosionPrefab;
 
 
 
-  void Start()
-  {
-    healthText.text = health.ToString();
-  }
+    void Start()
+    {
 
-  void OnTriggerEnter(Collider other)
-  {
-    GetComponent<AudioSource>().PlayOneShot(playerDamageSFX);
+        healthText.text = health.ToString();
+    }
 
-    health -= healthDecrease;
-    healthText.text = health.ToString();
+    void OnTriggerEnter(Collider other)
+    {
+        GetComponent<AudioSource>().PlayOneShot(playerDamageSFX);
 
-  }
+        health -= healthDecrease;
+        healthText.text = health.ToString();
+    }
+
+
+    public int getPlayerHealth()
+    {
+        return health;
+    }
+
 
 }
